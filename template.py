f1fc8924a0e5a6081c1ad46c0e37b326fd006687
ZhdankovAlex <47736968+ZhdankovAlex@users.noreply.github.com>
2021-01-20 16:24:27 +0300

    add sprite of player

--- template.py
+++ template.py
@@ -13,6 +13,25 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
+# Спрайт игрока
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 40))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
+    def update(self):
+        self.speedx = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -8
+        if keystate[pygame.K_RIGHT]:
+            self.speedx = 8
+        self.rect.x += self.speedx
+
 # Создаем игру и окно
 pygame.init()
 pygame.mixer.init()
